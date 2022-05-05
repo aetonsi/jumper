9fda581457aa4a576e01f6419c73fb80dad61458
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-05-05 15:30:21 +0200

    first commit

---
+++ jumper/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("jumper")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("jumper")> 
+<Assembly: AssemblyCopyright("")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3fa9d12f-d619-4574-8c93-a3a09026dbaa")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.2")> 
+<Assembly: AssemblyFileVersion("1.0.0.2")> 
---
+++ jumper/frmMain.vb
@@ -0,0 +1,30 @@
+﻿Module frmMain
+
+    Sub Main()
+        If My.Application.CommandLineArgs.Count = 0 Then
+            Exit Sub
+        End If
+        Dim p As New Process
+        Dim args As String = ""
+        'first arg received is process path
+        For i = 1 To My.Application.CommandLineArgs.Count - 1
+            args &= " " & qin(My.Application.CommandLineArgs(i))
+        Next
+        p.StartInfo.Arguments = args
+        p.StartInfo.FileName = My.Application.CommandLineArgs(0)
+
+        'p.StartInfo.UseShellExecute = False
+        'p.StartInfo.RedirectStandardError = p.StartInfo.RedirectStandardInput = p.StartInfo.RedirectStandardOutput = True
+
+        p.Start()
+    End Sub
+    Function qin(ByVal str As String) As String
+        If str.Contains(Chr(34)) Then
+            str = str.Replace(Chr(34), "\""")
+        End If
+        If str.Contains(" ") Then
+            str = """" & str & """"
+        End If
+        Return str
+    End Function
+End Module
